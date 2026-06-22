ba7628da984933c5aa364c31eb41d645908de45c
Vishnu <vishnusvprasad@gmail.com>
2026-06-22 22:23:35 +0530

    created contact us page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './Components/Navbar'
 import Footer from './Components/Footer'
+import Contact from './pages/Contact'
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-black flex flex-col">
-      <Navbar/>
-      {/* Main content will go here, using flex-grow to push footer down */}
-      <main className="flex-grow">
-      </main>
-      <Footer/>
-    </div>
+    <BrowserRouter>
+      <div className="min-h-screen bg-black flex flex-col">
+        <Navbar/>
+        {/* Main content will go here, using flex-grow to push footer down */}
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<div className="h-[50vh] flex items-center justify-center text-gray-500"><p>Click 'Get Started' to open the Contact Page.</p></div>} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer/>
+      </div>
+    </BrowserRouter>
   )
 }
 
